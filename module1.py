df8c51c599e9e50be41be466829d2161cd42dcab
Ram Basnet <rambasnet@gmail.com>
2017-09-27 20:24:50 -0600

    Added Ch12-Modules, module1 and module2.py

---
+++ module1.py
@@ -0,0 +1,20 @@
+"""
+This is module 1. Declares question, answer, and
+areaOfCircle function
+"""
+import math
+import module2
+
+question = "What is the meaning of Life, the Universe, and Everything?"
+answer = 42
+
+def areaOfCircle(radius):
+    """
+        Given radius of a circle, finds and returns its area.
+    """
+    return math.pi*radius**2
+
+print(question)
+print(answer)
+area = areaOfCircle(3)
+print("area = {:.2f}".format(area))
---
+++ module2.py
@@ -0,0 +1,19 @@
+"""
+This is module 2. Defines question, answer and
+circumference function
+"""
+import math
+import module1
+
+question = "What is your quest?"
+answer = "To seek the holy grail."
+
+def circumference(radius):
+    """
+        Given radius of a circle, finds and returns its circumference.
+    """
+    return math.pi*radius*2
+
+print(question)
+print(answer)
+print("circumference = {:.2f}".format(circumference(3)))

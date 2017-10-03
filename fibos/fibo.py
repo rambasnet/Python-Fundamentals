3ebbdb18d77243fb5b6098589318e93d2e3f3f7e
Ram Basnet <rambasnet@gmail.com>
2017-10-02 23:36:33 -0600

    added fibos package and fibo module

---
+++ fibos/fibo.py
@@ -0,0 +1,33 @@
+# Fibonacci numbers module
+
+def fib(n):    
+    """
+    write Fibonacci series up to nth term
+    """
+    a, b = 0, 1
+    print(a, b, end=' ')
+    i = 2
+    while i < n:
+        a, b = b, a+b
+        print(b, end=' ')
+        i += 1
+    print()
+
+def fib2(n):   
+    """
+    return list of Fibonacci series up to nth term
+    """
+    result = [0, 1]
+    i = 2
+    while i < n:
+        result.append(result[i-1] + result[i-2])
+        i += 1
+    return result
+
+
+if __name__ == "__main__":
+    import sys
+    fib(5)
+    print(fib2(5))
+    fib(int(sys.argv[1]))
+    print(fib2(int(sys.argv[1])))

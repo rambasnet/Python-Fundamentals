840e579b596c078ec9ba81554076958e88917928
Ram Basnet <rambasnet@gmail.com>
2017-10-23 22:10:31 -0600

    Added fib_unittest.pyin utility package

---
+++ utility/fib_unittest.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+import unittest
+from fib import fib
+
+class FibTest(unittest.TestCase):
+    # optional
+    def setUp(self):
+        self.fibSeq = [(1, 1), (2, 1), (3, 2), (10, 55), (20, 6765)]
+        print('setUp executed...')
+    
+
+    def test1(self):
+        print('test1')
+        for n, val in self.fibSeq:
+            self.assertEqual(fib(n), val)
+        
+        self.assertRaises(ValueError, fib, -1)
+    
+    def test2(self):
+        print('test2')
+        # function name can be arbitrary but must start with test or
+        # should be runTest
+        # assertEqual is defined in TestCase class
+        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib(10), 55)
+        self.assertEqual(fib(20), 6765)
+    
+    def test3(self):
+        #self.assertAlmostEqual(22/7, 3.142857, 6)
+        #self.assertEqual(22/7, 3.142857)
+        pass
+
+    # optional
+    def tearDown(self):
+        self.fibElems = None
+        print("tearDown executed...")
+
+
+if __name__ == "__main__":
+    unittest.main()

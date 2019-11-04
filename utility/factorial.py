d9741e1018cc9bb0aa8c3976a64fd901188555ff
Ram Basnet <rambasnet@gmail.com>
2019-11-04 10:09:37 -0700

    updated utility/factorial.py

--- utility/factorial.py
+++ utility/factorial.py
@@ -36,30 +36,42 @@ def factorial(n):
     OverflowError: n too large
     """
     import math
-    if type(n) != int:
-        raise TypeError("an integer is required.")
     if n < 0:
         raise ValueError("n must be >= 0")
     if math.floor(n) != n:
         raise ValueError("n must be exact integer")
+
     if n+1 == n:  # catch a value like 1e300
         raise OverflowError("n too large")
+    
     fact = 1
+    n = int(n)
     for i in range(1, n+1):
         fact *= i
     return fact
 
-def _test():
+def _test1():
+    print('test cases with assert statement called')
     assert factorial(1) == 1, "1! != 1"
     assert factorial(5) == 120, "5! != 120"
     assert factorial(20) == 2432902008176640000
+    
     # factorial of -ve integer
     # factorial of non-integer
+    print('all test cases passed!')
 
-def _test1():
+def _test2():
+    print("docttest.testmod() called")
     import doctest
     doctest.testmod()
+    print('all test cases passed!')
 
 if __name__ == "__main__":
-    #_test()
-    _test1()
+    import sys
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'test1':
+            _test1()
+        else:
+            _test2()
+    else:
+        print('Usage: python factorial.py [test1/test2]')

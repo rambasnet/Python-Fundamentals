45a4f71441420d1cac9c64f1b5791633518f4005
Ram Basnet <rambasnet@gmail.com>
2019-11-05 23:38:23 -0700

    updated fib_unittest.py

--- utility/fib_unittest.py
+++ utility/fib_unittest.py
@@ -8,18 +8,19 @@ from fib import fib
 
 
 class FibTest(unittest.TestCase):
-    # optional - executes once and the first
+    # optional - executes before each test function
     def setUp(self):
         self.fibSeq = [(1, 1), (2, 1), (3, 2), (10, 55), (20, 6765)]
         print('setUp executed...')
 
+    # any function that start with test* will be executed automatically
     def test1(self):
         print('test1')
         # assertEqual is defined in TestCase class
         for n, val in self.fibSeq:
             self.assertEqual(fib(n), val)
 
-        self.assertRaises(ValueError, fib, -1)
+        #self.assertRaises(ValueError, fib, -1)
 
     def test2(self):
         print('test2')
@@ -30,10 +31,21 @@ class FibTest(unittest.TestCase):
         self.assertEqual(fib(20), 6765)
 
     def test3(self):
-        print('test 3')
+        print('test3')
+        # doesn't test fib function but gets called...
         self.assertAlmostEqual(22/7, 3.142857, 6)
-        #self.assertEqual(22/7, 3.142857)
+        # uncomment the following and run it
+        # self.assertEqual(22/7, 3.142857)
         pass
+
+    def testException(self):
+        print('test exception')
+        # notice function is not called but is is passed along with the parameter
+        self.assertRaises(ValueError, fib, -1)
+
+    def someTest(self):
+        print('someTest')
+        print('This function will not run!')
 
     # optional - executes after each test function is executed
     def tearDown(self):

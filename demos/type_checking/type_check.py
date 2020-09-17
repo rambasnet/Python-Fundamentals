808a41a74ad5df87dd8f4c0c4996c89cd53ada72
Ram Basnet <rambasnet@gmail.com>
2020-09-16 21:07:04 -0600

    added settings.json

---
+++ demos/type_checking/type_check.py
@@ -0,0 +1,19 @@
+"""Script to demonstrate typing in Python 3.5 and higher version.
+
+Must include linting in VS Code.
+"""
+
+# Variable type annotations
+num: int = 10
+var: float = "Hello"
+
+name: str = "James Bond"
+var1: str = 100
+
+def greeting(name: str) -> str:
+    return f'Hello, {name}!'
+
+print(greeting('James Bond'))
+print(greeting(3434.3434))
+
+

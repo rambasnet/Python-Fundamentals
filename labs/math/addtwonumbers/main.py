67b856b51c23de3c904f520cb607962f98247cd4
Ram Basnet <rambasnet@gmail.com>
2023-09-05 11:19:28 -0600

    update addtwonumbers

--- labs/math/addtwonumbers/main.py
+++ labs/math/addtwonumbers/main.py
@@ -7,9 +7,9 @@ Date: FIXME
 Read and solve: Add Two Numbers - https://open.kattis.com/problems/addtwonumbers 
  
 Algorithm steps:
-	1. Read data as a line
-	2. Split the line into two integers
-	3. Add them up
+  1. Read data as a line
+  2. Split the line into two integers
+  3. Add them up
   4. print the result
 """
  
@@ -25,7 +25,7 @@ def main():
   print(f'{a=}, {b=}') # FIXME 1: comment/remove this line before submitting to Kattis
   # FIXME 2: convert string a into integer
   # FIXME 3: convert string b into integer
-	# FIXME 4: add two numbers and store the result into ans variable
-	# FIXME 5: print the answer as shown in the sample output
+  # FIXME 4: add two numbers and store the result into ans variable
+  # FIXME 5: print the answer as shown in the sample output
 
-main() # call smain function
+main() # call main function

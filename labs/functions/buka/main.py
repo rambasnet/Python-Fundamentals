d930e45b6015ec4b03e3ee204c865a43bbb58d78
Ram Basnet <rambasnet@gmail.com>
2023-09-11 12:49:50 -0600

    update buka main.py

--- labs/functions/buka/main.py
+++ labs/functions/buka/main.py
@@ -21,13 +21,13 @@ import sys
 def main():
   """Main function that solves the problem
   """
-  # read/input the first operand into A variable
-  A = input()
-  # FIXME3 - read/input the 2nd line into operator variable
-  # FIXME4 - read/input the 2nd operand into B variable
+  # read/input the first operand into A variable using sys library's readline() method
+  A = sys.stdin.readline().strip()
+  # FIXME3 - using sys library, read/input the 2nd line into operator variable
+  # FIXME4 - using sys library, read/input the 2nd operand into B variable
   # FIXME5 - concatenate all three variables into a single variable called equation
   # call eval(equation) and assign the return value or result into ans variable
-  ans = '' # FIXME6 - find the answer using eval function
+  ans = 'FIXME' # FIXME6 - find the answer using eval function
   # print the answer using sys library's stdout.write() method
   sys.stdout.write(f'{ans}\n')
 

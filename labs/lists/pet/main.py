a6ba8abce407fd81a3fe91cf2899e701c2982c4f
Ram Basnet <rambasnet@gmail.com>
2023-09-11 18:16:24 -0600

    add separate test modules

--- labs/lists/pet/main.py
+++ labs/lists/pet/main.py
@@ -67,33 +67,5 @@ def answer(scores: List[int]):
   # FIXME 8: return the index+1 and the max number in the list as a string
   pass
 
-# test function must start with test_ prefix for pytest to recognize it
-def test_list_sum():
-  """Tests list_sum function
-  """
-  ans = list_sum([6, 7, 8, 10])
-  expected = 31
-  assert ans == expected, f"Expected: {expected}, but got: {ans}"
-  ans = list_sum([2, 3, 4, 5])
-  expected = 14
-  assert ans == expected, f"Expected: {expected}, but got: {ans}"
-  # FIXME 9: add 2 more test cases
-  print("All test cases passed...", file=sys.stderr)
-
-
-# test function must start with test_ prefix for pytest to recognize it
-def test_answer():
-  """Tests answer function
-  """
-  ans = answer([10, 20, 11, 15, 13])
-  expected = "2 20"
-  assert ans == expected, f"Expected: {expected}, but got: {ans}"
-  ans = answer([6, 10, 8, 4, 15])
-  expected = "5 15"
-  assert ans == expected, f"Expected: {expected}, but got: {ans}"
-  # FIXME 10: add 2 more test cases
-  print("All test cases passed...", file=sys.stderr)
-
-
 if __name__ == "__main__":
   main()
